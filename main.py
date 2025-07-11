a225ead1226cfd8d6c460b3fa8635305ab377bd7
Tanner Terry <tannerterry221@gmail.com>
2025-07-11 06:39:17 -0600

    Add robust SPY weight mapping, retried formats

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, Request, RedirectResponse
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
